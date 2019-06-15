5ec8d2d04d2ba4afe2ac171de495803439d9d91a
RVIRUS0817 <t.virus.1989@gmail.com>
2019-06-15 17:34:51 +0900

    add update-repo.sh

---
+++ docker/scripts/update-repo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ~/www/future-architect/vuls
+git pull
+
+cd ~/www/knqyf263/gost
+git pull
+
+cd ~/www/mozqnet/go-exploitdb/
+git pull
+
+cd ~/www/kotakanbe/go-cve-dictionary
+git pull
+
+cd ~/www/kotakanbe/goval-dictionary/
+git pull
